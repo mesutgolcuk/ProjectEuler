43b28ef8effccc420b5137334e45da724013c30a
Mesut GÖLCÜK <mesutgolcuk@gmail.com>
2016-08-04 14:09:10 +0300

    Update Problem 1.c

--- Problem 1.c
+++ Problem 1.c
@@ -1,21 +1,20 @@
-			//Mesut GÖLCÜK
-			//26.2.2014 23:10
+	//Mesut GÃ–LCÃœK
+	//26.2.2014 23:10
 #include<stdio.h>
 
 int main(){
 	int sum=0;
 	int i=0;
 
-		while(i<1000){
-			if(i%3==0){
-				sum=sum+i;
-			}
-			else if(i%5==0){
-				sum=sum+i;
-			}
-
-			i++;
+	while(i<1000){
+		if(i%3==0){
+			sum=sum+i;
 		}
+		else if(i%5==0){
+			sum=sum+i;
+		}
+		i++;
+	}
 
 	printf("Toplam: %d",sum);
 	return 0;
